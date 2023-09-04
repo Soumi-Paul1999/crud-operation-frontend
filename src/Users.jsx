02c85b7ecaa263fb2449dbf7265383f6e5b3872c
Soumi Paul <spmamon1999@gmail.com>
2023-09-04 18:20:53 +0530

    get api test

--- src/Users.jsx
+++ src/Users.jsx
@@ -6,7 +6,7 @@ const Users = () => {
     const [users,setUsers] = useState([])
 
     useEffect(()=>{
-        axios.get("http://localhost:3001")
+        axios.get("https://crud-operation-backend.vercel.app")
         .then(result=> setUsers(result.data))
         .catch(err=>console.log(err))
     },[])
